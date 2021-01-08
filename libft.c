348aa81e47994639cd65df8a5fce6c07c5e95555
pflorent <isatis.tempillon@gmail.com>
2021-01-08 14:17:05 +0100

    newzzz

--- libft.c
+++ libft.c
@@ -424,7 +424,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 	lgt = (char_count(s1, set) + 1);
 	if(!(dest = malloc(lgt * sizeof(char)))) 
         return (NULL);
-	
 	while (*s1)
 	{
 		while(set[i] && set[i] != *s1)
@@ -440,17 +439,60 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 /*///////////////////////////////////////// A TESTER /////////////////////////////////////////////////////////*/
 
-void	free_all()
+void	free_all(char **to_free)
 {
+	int i;
+
+	i = -1;
+	while(to_free[++i])
+		free(to_free[i]);
+	free(to_free);
+}
+
+int		words_count(char const *s, char sep)
+{
+	int i;
+	int count;
 
 
-	
+	i = -1;
+	while(s[++i])
+	{
+
+
+
+	}
 }
 
 char	**ft_split(char const *s, char c)
 {
-	
+	int i;
+	int count;
+	int len;
+	char **dest;
 
+	i = -1;
+	count = words_count(s, c);
+	if(!count)
+	{
+		if(!(dest[0] = malloc(sizeof(char)))) 
+        	return (NULL);
+		*dest[0] = '\0';
+		return (dest);
+	}
+	while(s[++i])
+	{
+		while(s[i] == c)
+			i++;
+		if(s[i])
+		{
+
+
+
+
+
+		}
+	}
 }
 
 /*/////////////////////////////////////////// A FAIRE ///////////////////////////////////////////////////////*/
