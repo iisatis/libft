25c28dd6f83869669578287101fa03a3c0eb751c
Patrick FLORENTIN <pflorent@e3r7p22.42.fr>
2021-01-10 15:55:39 +0100

    neozzzzzzzzzzzzzzzzzzz

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,21 +6,26 @@
 /*   By: pflorent <pflorent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 16:32:35 by pflorent          #+#    #+#             */
-/*   Updated: 2021/01/09 17:22:32 by pflorent         ###   ########.fr       */
+/*   Updated: 2021/01/10 15:53:33 by pflorent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t		ft_strlcpy(char *dest, const char *src, int lgt)
+size_t		ft_strlcpy(char *dest, const char *src, size_t lgt)
 {
-	int i;
-	int n;
+	size_t i;
+	size_t n;
 
-	i = -1;
+	i = 0;
 	n = ft_strlen((char *)src);
-	while (src[++i] && i < (lgt - 1))
+	if (!lgt)
+		return (n);
+	while (src[i] && i < (lgt - 1))
+	{
 		dest[i] = src[i];
+		i++;
+	}
 	dest[i] = '\0';
 	return (n);
 }
