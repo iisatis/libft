9fbbda8020229ddf18cc68ae022bb1ed0eb685b0
Patrick FLORENTIN <pflorent@e3r1p4.42.fr>
2021-01-11 16:52:35 +0100

    halffffffffff

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: pflorent <pflorent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 16:26:16 by pflorent          #+#    #+#             */
-/*   Updated: 2021/01/10 17:55:43 by pflorent         ###   ########.fr       */
+/*   Updated: 2021/01/11 16:52:25 by pflorent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,17 @@
 int		ft_memcmp(const void *p1, const void *p2, size_t size)
 {
 	size_t			i;
-	int				r;
 	unsigned char	*p1_c;
 	unsigned char	*p2_c;
 
 	i = 0;
-	r = 0;
 	p1_c = (unsigned char *)p1;
 	p2_c = (unsigned char *)p2;
-	while (p1_c[i] == p2_c[i] && i < size)
+	while (size--)
+	{
+		if (p1_c[i] != p2_c[i])
+			return (p1_c[i] - p2_c[i]);
 		i++;
-	return (p1_c[i] - p2_c[i]);
+	}
+	return (0);
 }
