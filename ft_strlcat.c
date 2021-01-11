64ae90972d6e7ead7e76d447609b39a0a64cd8d1
Patrick FLORENTIN <pflorent@e3r1p4.42.fr>
2021-01-11 18:26:44 +0100

    halfffffffffffff

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,31 +6,31 @@
 /*   By: pflorent <pflorent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 16:43:40 by pflorent          #+#    #+#             */
-/*   Updated: 2021/01/11 16:23:13 by pflorent         ###   ########.fr       */
+/*   Updated: 2021/01/11 18:25:42 by pflorent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t        ft_strlcat(char *dest, const char *src, size_t len)
+size_t	ft_strlcat(char *dest, const char *src, size_t len)
 {
-    size_t	dest_size;
-    size_t	i;
+	size_t dest_size;
+	size_t i;
 
-    dest_size = ft_strlen((char *)src) + ft_strlen(dest);
-    i = 0;
+	dest_size = ft_strlen((char *)src) + ft_strlen(dest);
+	i = 0;
 	if (len <= ft_strlen(dest))
 		return (ft_strlen((char *)src) + len);
-    while (*dest)
-    {
-        dest++;
-        len --;
-    }
-    while (i < (len - 1) && src[i])
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-    return (dest_size);
+	while (*dest)
+	{
+		dest++;
+		len--;
+	}
+	while (i < (len - 1) && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest_size);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: pflorent <pflorent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 16:49:08 by pflorent          #+#    #+#             */
-/*   Updated: 2021/01/11 18:20:11 by pflorent         ###   ########.fr       */
+/*   Updated: 2021/01/11 18:26:26 by pflorent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,17 @@ char	*ft_strtrim(char const *s1, char const *set)
 	start = 0;
 	end = ft_strlen((char *)s1);
 	i = 0;
-	if (start == end)
+	while (ft_strchr(set, s1[start]))
+		start++;
+	while (ft_strchr(set, s1[end]) && end)
+		end--;
+	if (start >= end)
 	{
 		if (!(dest = malloc(sizeof(char))))
 			return (NULL);
 		dest[0] = '\0';
 		return (dest);
-	}		
-	while(ft_strchr(set, s1[start]))
-		start++;
-	while (ft_strchr(set, s1[end]))
-		end--;
+	}
 	if (!(dest = malloc((end - start + 2) * sizeof(char))))
 		return (NULL);
 	ft_strlcpy(dest, &s1[start], (end - start + 2));
