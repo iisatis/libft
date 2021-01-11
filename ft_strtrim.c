201b018c151f26c95e0ac5797abf8e2158b5c7b3
Patrick FLORENTIN <pflorent@e3r1p4.42.fr>
2021-01-11 18:20:22 +0100

    halffffffffffff

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: pflorent <pflorent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 16:49:08 by pflorent          #+#    #+#             */
-/*   Updated: 2021/01/11 17:58:12 by pflorent         ###   ########.fr       */
+/*   Updated: 2021/01/11 18:20:11 by pflorent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,19 @@ char	*ft_strtrim(char const *s1, char const *set)
 	start = 0;
 	end = ft_strlen((char *)s1);
 	i = 0;
+	if (start == end)
+	{
+		if (!(dest = malloc(sizeof(char))))
+			return (NULL);
+		dest[0] = '\0';
+		return (dest);
+	}		
 	while(ft_strchr(set, s1[start]))
 		start++;
 	while (ft_strchr(set, s1[end]))
 		end--;
 	if (!(dest = malloc((end - start + 2) * sizeof(char))))
 		return (NULL);
-	while (start <= end)
-	{
-		dest[i] = (char)s1[start];
-		start++;
-		i++;
-	}
-	dest[i] = '\0';
+	ft_strlcpy(dest, &s1[start], (end - start + 2));
 	return (dest);
 }
