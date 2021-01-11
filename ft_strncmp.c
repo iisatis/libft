145361898fe628832618f9baae27c8bf368ba78f
Patrick FLORENTIN <pflorent@e3r1p4.42.fr>
2021-01-11 18:35:46 +0100

    halffffffffffffff

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: pflorent <pflorent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 16:31:49 by pflorent          #+#    #+#             */
-/*   Updated: 2021/01/10 17:05:12 by pflorent         ###   ########.fr       */
+/*   Updated: 2021/01/11 18:31:29 by pflorent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,9 @@
 int		ft_strncmp(const char *first_str, const char *second_str, size_t lgt)
 {
 	size_t	i;
-	int		r;
 
 	i = 0;
-	r = 0;
 	while (first_str[i] && (first_str[i] == second_str[i]) && i < lgt)
 		i++;
-	return ((unsigned char)(first_str[i] - second_str[i]));
+	return ((int)(first_str[i] - second_str[i]));
 }
