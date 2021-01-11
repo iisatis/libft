33706413e1c2ea31f6a4b77ae451f1cef424e508
Patrick FLORENTIN <pflorent@e3r1p4.42.fr>
2021-01-11 16:23:38 +0100

    halffffffff

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: pflorent <pflorent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 16:43:40 by pflorent          #+#    #+#             */
-/*   Updated: 2021/01/10 18:40:58 by pflorent         ###   ########.fr       */
+/*   Updated: 2021/01/11 16:23:13 by pflorent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,10 @@ size_t        ft_strlcat(char *dest, const char *src, size_t len)
 	if (len <= ft_strlen(dest))
 		return (ft_strlen((char *)src) + len);
     while (*dest)
+    {
         dest++;
+        len --;
+    }
     while (i < (len - 1) && src[i])
     {
         dest[i] = src[i];
