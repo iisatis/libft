d038440e5ff90ebda15f44af19c4bbd59f3a1506
Patrick FLORENTIN <pflorent@e3r5p18.42.fr>
2021-01-12 17:43:10 +0100

    splitttttttt

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pflorent <pflorent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/09 15:42:59 by pflorent          #+#    #+#             */
-/*   Updated: 2021/01/10 18:39:16 by pflorent         ###   ########.fr       */
+/*   Updated: 2021/01/12 17:42:43 by pflorent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,39 +16,45 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-void	*ft_memset(void *p, int value, size_t size);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t size);
-void	*ft_memccpy(void *dest, const void *src, int ch, size_t size);
-void	*ft_memmove (void *dest, const void *src, size_t size);
-void	*ft_memchr (const void *mem_block, int ch, size_t size);
-int		ft_memcmp (const void *p1, const void *p2, size_t size);
-size_t	ft_strlen(char *str);
-int		ft_isalpha(int a);
-int		ft_isdigit(int a);
-int		ft_isalnum(int a);
-int		ft_isascii(int a);
-int		ft_isprint(int a);
-int		ft_toupper(int a);
-int		ft_tolower(int a);
-char	*ft_strchr(const char *str, int chr);
-char	*ft_strrchr(const char *str, int chr);
-int		ft_strncmp(const char *first_str, const char *second_str, size_t lgt);
-size_t	ft_strlcpy(char *dest, const char *src, size_t lgt);
-size_t  ft_strlcat(char *dest, const char *src, size_t len);
-char	*ft_strnstr(const char *str, const char *find, size_t lgt);
-int		ft_atoi(const char *str);
-void	*ft_calloc(size_t elem_count, size_t elem_size);
-char	*ft_strdup(const char *src);
-char	*ft_strndup(const char *src, unsigned int len);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strtrim(char const *s1, char const *set);
-char	*ft_itoa(int n);
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putendl_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
-char	**ft_split(char const *s, char c);
+typedef struct	s_list
+{
+	void			*content;
+	struct s_list	*next;
+}				t_list;
+
+void			*ft_memset(void *p, int value, size_t size);
+void			ft_bzero(void *s, size_t n);
+void			*ft_memcpy(void *dest, const void *src, size_t size);
+void			*ft_memccpy(void *dest, const void *src, int ch, size_t size);
+void			*ft_memmove (void *dest, const void *src, size_t size);
+void			*ft_memchr (const void *mem_block, int ch, size_t size);
+int				ft_memcmp (const void *p1, const void *p2, size_t size);
+size_t			ft_strlen(char *str);
+int				ft_isalpha(int a);
+int				ft_isdigit(int a);
+int				ft_isalnum(int a);
+int				ft_isascii(int a);
+int				ft_isprint(int a);
+int				ft_toupper(int a);
+int				ft_tolower(int a);
+char			*ft_strchr(const char *str, int chr);
+char			*ft_strrchr(const char *str, int chr);
+int				ft_strncmp(const char *s1, const char *s2, size_t lgt);
+size_t			ft_strlcpy(char *dest, const char *src, size_t lgt);
+size_t			ft_strlcat(char *dest, const char *src, size_t len);
+char			*ft_strnstr(const char *str, const char *find, size_t lgt);
+int				ft_atoi(const char *str);
+void			*ft_calloc(size_t elem_count, size_t elem_size);
+char			*ft_strdup(const char *src);
+char			*ft_strndup(const char *src, unsigned int len);
+char			*ft_substr(char const *s, unsigned int start, size_t len);
+char			*ft_strjoin(char const *s1, char const *s2);
+char			*ft_strtrim(char const *s1, char const *set);
+char			*ft_itoa(int n);
+char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void			ft_putchar_fd(char c, int fd);
+void			ft_putstr_fd(char *s, int fd);
+void			ft_putendl_fd(char *s, int fd);
+void			ft_putnbr_fd(int n, int fd);
+char			**ft_split(char const *s, char c);
 #endif
