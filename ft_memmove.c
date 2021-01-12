8c148461cad17a325c98b8857ba0412ac79d696e
Patrick FLORENTIN <pflorent@e3r5p18.42.fr>
2021-01-12 17:28:07 +0100

    splitttttt

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,48 +6,23 @@
 /*   By: pflorent <pflorent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 16:24:51 by pflorent          #+#    #+#             */
-/*   Updated: 2021/01/11 16:54:12 by pflorent         ###   ########.fr       */
+/*   Updated: 2021/01/12 17:27:47 by pflorent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t size)
+void	*ft_memmove(void *dest, const void *src, size_t len)
 {
-	size_t				i;
-	unsigned char		temp[size + 1];
-	unsigned char		*d;
-	const unsigned char	*s;
+	size_t	i;
+	char	*s;
+	char	*d;
 
-	d = (unsigned char *)dest;
-	s = (const unsigned char *)src;
+	s = (char *)src;
+	d = (char *)dest;
 	i = 0;
 	if (dest == NULL && src == NULL)
 		return (NULL);
-	while (i < size)
-	{
-		temp[i] = s[i];
-		i++;
-	}
-	i = 0;
-	while (i < size)
-	{
-		d[i] = temp[i];
-		i++;
-	}
-	return (dest);
-}
-
-/*
-void	*ft_memmove(void *dst, const void *src, size_t len)
-{
-	char	*s;
-	char	*d;
-	size_t	i;
-
-	s = (char *)src;
-	d = (char *)dst;
-	i = 0;
 	if (d > s)
 		while (len-- > 0)
 			d[len] = s[len];
@@ -57,5 +32,5 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 			d[i] = s[i];
 			i++;
 		}
-	return (dst);
-}*/
+	return (dest);
+}
