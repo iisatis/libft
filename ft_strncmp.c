75b0b33145ebc7184230685bab5f29237b876ab9
Patrick FLORENTIN <pflorent@e3r5p18.42.fr>
2021-01-12 17:24:16 +0100

    splittttt

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,18 +6,21 @@
 /*   By: pflorent <pflorent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 16:31:49 by pflorent          #+#    #+#             */
-/*   Updated: 2021/01/11 18:31:29 by pflorent         ###   ########.fr       */
+/*   Updated: 2021/01/12 17:24:03 by pflorent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_strncmp(const char *first_str, const char *second_str, size_t lgt)
+int		ft_strncmp(const char *s1, const char *s2, size_t len)
 {
 	size_t	i;
 
 	i = 0;
-	while (first_str[i] && (first_str[i] == second_str[i]) && i < lgt)
+	while (s1[i] && s2[i] && ((unsigned char)s1[i] == (unsigned char)s2[i])
+			&& i < len)
 		i++;
-	return ((int)(first_str[i] - second_str[i]));
+	if (i == len)
+		return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
