0ba4e7378bc9668058205c1307ad1c47dfef96ba
Patrick FLORENTIN <pflorent@e3r2p12.42.fr>
2021-01-13 16:22:07 +0100

    newbbbbbbbbbb

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: pflorent <pflorent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 16:14:51 by pflorent          #+#    #+#             */
-/*   Updated: 2021/01/13 16:15:28 by pflorent         ###   ########.fr       */
+/*   Updated: 2021/01/13 16:20:17 by pflorent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,13 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-
-
-
-
-	
+	if (lst != NULL)
+	{
+		while (lst->next != NULL)
+		{
+			f(lst->content);
+			lst = lst->next;
+		}
+		f(lst->content);
+	}
 }
