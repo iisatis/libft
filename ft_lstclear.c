5e1dd50cca42640da76df62ae232c57e8a9ba120
Patrick FLORENTIN <pflorent@e3r2p12.42.fr>
2021-01-13 16:14:08 +0100

    newbbbbbbbb

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: pflorent <pflorent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 15:59:54 by pflorent          #+#    #+#             */
-/*   Updated: 2021/01/13 16:04:41 by pflorent         ###   ########.fr       */
+/*   Updated: 2021/01/13 16:13:25 by pflorent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,11 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
 	t_list *del_lst;
 
-	del_lst = *lst;
-	while(del_lst->next != NULL)
-	{
-		ft_lstdelone(del_lst, del);
-		del_lst = del_lst->next;
-	}
+	if(lst != NULL)
+		while (*lst != NULL)
+		{
+			del_lst = *lst;
+			ft_lstdelone(*lst, del);
+			*lst = del_lst->next;
+		}
 }
