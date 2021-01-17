53f32fd0f65de6b0add475ebebc12c2651301064
pflorent <isatis.tempillon@gmail.com>
2021-01-17 22:09:51 +0100

    try2

--- ft_split.c
+++ ft_split.c
@@ -12,6 +12,94 @@
 
 #include "libft.h"
 
+void	free_all(char **to_free, int n)
+{
+	while (--n >= 0)
+		free(to_free[n]);
+	free(to_free);
+}
+
+size_t	word_len(const char *s, char sep)
+{
+	int i;
+
+	i = 0;
+	while (s[i] && s[i] != sep)
+		i++;
+	return (i);
+}
+
+size_t	words_count(char const *s, char sep)
+{
+	size_t i;
+	size_t count;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == sep)
+			i++;
+		if (s[i])
+		{
+			count++;
+			i += word_len(&s[i], sep);
+		}
+	}
+	return (count);
+}
+
+int		fill_tab(char **dest, char const *s, char c, size_t words)
+{
+	int i;
+
+	i = 0;
+	while (words)
+	{
+		while (*s == (const char)c)
+			s++;
+		if (!(dest[i] = malloc(word_len(s, c) + 1 * sizeof(char))))
+			return (i);
+		if (!(ft_strlcpy(dest[i], s, (word_len(s, c) + 1))))
+			return (i + 1);
+		while (*s && *s != (const char)c)
+			s++;
+		words--;
+		i++;
+	}
+	*dest = 0;
+	return (-1);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	size_t	count;
+	char	**dest;
+	int		to_free;
+
+	count = words_count(s, c);
+	if (!(dest = malloc(sizeof(char*) * (count + 1))))
+		return (NULL);
+	if (!count)
+	{
+		if (!(dest = malloc(sizeof(char*))))
+		{
+			free(dest);
+			return (NULL);
+		}
+		*dest = 0;
+		return (dest);
+	}
+	to_free = fill_tab(dest, s, c, count);
+	if (to_free != -1)
+	{
+		free_all(dest, to_free);
+		return (NULL);
+	}
+	return (dest);
+}
+
+/*
 void	free_all(char **to_free)
 {
 	int i;
@@ -93,3 +181,4 @@ char	**ft_split(char const *s, char c)
 	}
 	return (dest);
 }
+*/
